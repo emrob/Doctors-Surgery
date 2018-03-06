3c7ea076973c921d454c3bf6f002d9b8e54f1104
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-06 21:49:16 +0000

    Methods added to Surgery class

--- src/main/java/Surgery.java
+++ src/main/java/Surgery.java
@@ -2,23 +2,42 @@ import java.util.ArrayList;
 
 public class Surgery {
 
+    private Patient patient;
+    private Doctor doctor;
+    private Nurse nurse;
+    private ArrayList<People> waitingRoom;
+    private ArrayList<People> doctorsRoom;
 
+
+    public Surgery(Patient patient, Doctor doctor, Nurse nurse,ArrayList<People> waitingRoom,ArrayList<People> doctorsRoom){
+        this.patient = patient;
+        this.doctor = doctor;
+        this.nurse = nurse;
+        this.waitingRoom = waitingRoom;
+        this.doctorsRoom = doctorsRoom;
+    }
+
+    public int checkIn(){
+        waitingRoom.add(patient);
+        return waitingRoom.size();
+    }
+
+    public int doctorSeePatient(){
+        doctorsRoom.add(patient);
+        if (doctorsRoom.size() > 0)
+        waitingRoom.remove(patient);
+        return doctorsRoom.size();
+
+    }
+
+    public String doctorTreatPatient(){
+        if (patient.getSymptomlevel() == SymptomLevel.High)
+       return doctor.administer();
+       else
+           return "no medicine required";
+
+    }
 }
-
-
-//    private Patient patient;
-//    private Doctor doctor;
-//    private Nurse nurse;
-//    private ArrayList<Room> waitingRoom;
-//    private ArrayList<Room> doctorsRoom;
-//    private ArrayList<Room> nursesRoom;
-//
-//    public Surgery(Patient patient, Doctor doctor, Nurse nurse,ArrayList<Room> waitingRoom, ArrayList<Room> doctorsRoom, ArrayList<Room> nursesRoom){
-//        this.patient = patient;
-//        this.doctor = doctor;
-//        this.nurse = nurse;
-//    }
-//}
 
 
 
