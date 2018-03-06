97aaf0dd86b998c4b2f540edaf085404c25c0e95
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-06 21:59:21 +0000

    - this file was missed from latest commit

--- src/test/java/TestSurgery.java
+++ src/test/java/TestSurgery.java
@@ -1,11 +1,42 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestSurgery {
     Surgery surgery;
+    Patient patient;
+    Doctor doctor;
+    Nurse nurse;
+    ArrayList<People> waitingRoom;
+    ArrayList<People> doctorsRoom;
 
     @Before
     public void setup() {
-        surgery = new Surgery();
+        doctor = new Doctor("Dr Strathie", "full time");
+        patient = new Patient(SymptomLevel.Minor, "Miranda");
+        nurse = new Nurse("Nurse Barbara", "part time");
+        waitingRoom = new ArrayList<>();
+        waitingRoom.add(patient);
+        doctorsRoom = new ArrayList<>();
+
+        surgery = new Surgery(patient, doctor, nurse, waitingRoom, doctorsRoom);
     }
 
+    @Test
+    public void testCheckIn(){
+        assertEquals(2, surgery.checkIn());}
+
+    @Test
+    public void testSeePatient(){
+        assertEquals(1, surgery.doctorSeePatient());
+        assertEquals(1, surgery.checkIn());
+       }
+
+    @Test
+    public void testDocTreatPatient(){
+        assertEquals("no medicine required", surgery.doctorTreatPatient());
+    }
 }
